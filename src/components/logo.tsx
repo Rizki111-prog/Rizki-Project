28a5984027308d68f08b472d5dbb53206e20bb34
Rizki Saputra <rizkiworkofficial@gmail.com>
2026-01-04 08:12:14 +0000

    "Adjust the dimensions of the application logo within the sidebar header

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 
 export function Logo({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={cn('relative h-8 w-8', className)} {...props}>
+    <div className={cn('relative h-10 w-10', className)} {...props}>
         <Image 
             src="/logo.png" 
             alt="Rizki App Logo" 
             fill 
-            sizes="32px"
+            sizes="40px"
             className="object-contain"
         />
     </div>
